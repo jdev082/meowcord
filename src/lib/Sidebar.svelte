c1add4ef403038227f4e0833f9deb39a919b84b5
M.D. Walters <popularvsofficial@gmail.com>
2023-02-06 19:04:08 +0000

    Finally, a good sidebar

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -5,8 +5,34 @@
 </script>
 
 <div class="outer">
-    <div class="inner bg-body-secondary text-dark overflow-scroll">
+    <div class="inner bg-light text-light overflow-scroll">
+        <SidebarIcons>A</SidebarIcons>
+        <SidebarIcons>B</SidebarIcons>
+        <SidebarIcons>C</SidebarIcons>
+        <SidebarIcons>D</SidebarIcons>
         <SidebarIcons>E</SidebarIcons>
+        <SidebarIcons>F</SidebarIcons>
+        <SidebarIcons>G</SidebarIcons>
+        <SidebarIcons>H</SidebarIcons>
+        <SidebarIcons>I</SidebarIcons>
+        <SidebarIcons>J</SidebarIcons>
+        <SidebarIcons>K</SidebarIcons>
+        <SidebarIcons>L</SidebarIcons>
+        <SidebarIcons>M</SidebarIcons>
+        <SidebarIcons>N</SidebarIcons>
+        <SidebarIcons>O</SidebarIcons>
+        <SidebarIcons>P</SidebarIcons>
+        <SidebarIcons>Q</SidebarIcons>
+        <SidebarIcons>R</SidebarIcons>
+        <SidebarIcons>S</SidebarIcons>
+        <SidebarIcons>T</SidebarIcons>
+        <SidebarIcons>U</SidebarIcons>
+        <SidebarIcons>V</SidebarIcons>
+        <SidebarIcons>W</SidebarIcons>
+        <SidebarIcons>X</SidebarIcons>
+        <SidebarIcons>Y</SidebarIcons>
+        <SidebarIcons>Z</SidebarIcons>
+        <hr class="rounded border-secondary">
     </div>
 </div>
 
@@ -17,7 +43,7 @@
     }
     .inner {
         height: 100%;
-        width: 6%;
+        width: 5%;
         padding: 5px;
     }
 </style>
--- src/lib/SidebarIcons.svelte
+++ src/lib/SidebarIcons.svelte
@@ -1,12 +1,15 @@
 <!-- Buttons for the Sidebar -->
 
-<div class="rounded-circle bg-dark-subtle text-center button">
-    <slot></slot>
+<div class="rounded-circle bg-secondary bg-gradient text-center button position-relative fs-2 fw-bold">
+    <div class="position-absolute top-50 start-50 translate-middle">
+        <slot></slot>
+    </div>
 </div>
 
 <style>
     .button {
-        height: 75px;
-        width: 75px;
+        height: 60px;
+        width: 60px;
+        margin-bottom: 10px;
     }
 </style>
