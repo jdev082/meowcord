c600308055f178774261fe80b83b38180118aa69
M.D. Walters <popularvsofficial@gmail.com>
2023-02-13 03:55:42 +0000

    Forgor

--- src/lib/screens/Home.svelte
+++ src/lib/screens/Home.svelte
@@ -24,7 +24,7 @@
             let post_content = document.createElement("p");
 
             pfp.classList.add("float-start", "me-3");
-            pfp.innerHTML = `<img class="rounded-circle" width="50" height="50" alt="${post.u}'s profile picture" src="${await getProfilePicture(post.u)}">`;
+            pfp.innerHTML = `<img class="rounded-circle bg-light" width="50" height="50" alt="${post.u}'s profile picture" src="${await getProfilePicture(post.u)}">`;
             post.append(pfp);
 
             post_username.classList.add("mb-0");
@@ -32,23 +32,43 @@
             switch (data.val.u) {
                 case "Discord":
                     post_username.innerHTML = `<b class="badge text-light" style="background-color: #5865F2;">${data.val.p.split(": ")[0]}</b>`;
-                    post_content.innerText = data.val.p.slice(data.val.p.indexOf(": ") + 1);
+                    if (data.val.p.slice(data.val.p.indexOf(": ") + 1) == "") {
+                        post_content.innerHTML = "<br>";
+                    } else {
+                        post_content.innerText = data.val.p.slice(data.val.p.indexOf(": ") + 1);
+                    }
                     break;
                 case "Webhooks":
                     post_username.innerHTML = `<b class="badge bg-warning text-light">${data.val.p.split(": ")[0]}</b>`;
-                    post_content.innerText = data.val.p.slice(data.val.p.indexOf(": ") + 1);
+                    if (data.val.p.slice(data.val.p.indexOf(": ") + 1) == "") {
+                        post_content.innerHTML = "<br>";
+                    } else {
+                        post_content.innerText = data.val.p.slice(data.val.p.indexOf(": ") + 1);
+                    }
                     break;
                 case "gcbridge":
                     post_username.innerHTML = `<b class="badge text-light" style="background-color: #ffa200;">${data.val.p.split(": ")[0]}</b>`;
-                    post_content.innerText = data.val.p.slice(data.val.p.indexOf(": ") + 1);
+                    if (data.val.p.slice(data.val.p.indexOf(": ") + 1) == "") {
+                        post_content.innerHTML = "<br>";
+                    } else {
+                        post_content.innerText = data.val.p.slice(data.val.p.indexOf(": ") + 1);
+                    }
                     break;
                 case "Revower":
                     post_username.innerHTML = `<b class="badge bg-danger text-light">${data.val.p.split(": ")[0]}</b>`;
-                    post_content.innerText = data.val.p.slice(data.val.p.indexOf(": ") + 1);
+                    if (data.val.p.slice(data.val.p.indexOf(": ") + 1) == "") {
+                        post_content.innerHTML = "<br>";
+                    } else {
+                        post_content.innerText = data.val.p.slice(data.val.p.indexOf(": ") + 1);
+                    }
                     break;
                 default:
                     post_username.innerHTML = `<b>${data.val.u}</b>`;
-                    post_content.innerText = data.val.p;
+                    if (data.val.p.slice(data.val.p.indexOf(": ") + 1) == "") {
+                        post_content.innerHTML = "<br>";
+                    } else {
+                        post_content.innerText = data.val.p.slice(data.val.p.indexOf(": ") + 1);
+                    }
                     break;
             }
 
@@ -70,22 +90,33 @@
         </div>
     </div>
 {:then home}
+    <div class="chat-info fs-5 fw-bold text-wrap">
+        <p class="m-0 ps-3">Home <span class="text-muted">This is the place where everyone posts</span></p>
+    </div>
+
+    <br>
+
     <div id="home-posts">
         {#each home as post}
             <div>
                 {#if post.u == "Discord"}
                     <div class="float-start me-3">
-                        {#await getProfilePicture(post.u.split(": ")[0])}
+                        {#await getProfilePicture(post.p.split(": ")[0])}
                             <div class="rounded-circle bg-light" style="width: 50; height: 50;"></div>
                         {:then pfp}
-                            <img class="rounded-circle bg-light" width="50" height="50" alt="{post.u}'s profile picture" src={pfp} onerror="this.src='./pfp/icon_err.svg'">
+                            <div class="rounded-circle bg-light">
+                                <img width="50" height="50" alt="{post.u}'s profile picture" src={pfp} onerror="this.src='./pfp/icon_err.svg'">
+                                <span class="position-absolute top-100 start-100 translate-middle badge rounded-pill bg-success p-2">
+                                    <span></span>
+                                </span>
+                            </div>
                         {/await}
                     </div>
                     <b class="badge text-light" style="background-color: #5865F2;">{post.p.split(": ")[0]}</b>
                     <p>{post.p.slice(post.p.indexOf(": ") + 1)}</p>
                 {:else if post.u == "Webhooks"}
                     <div class="float-start me-3">
-                        {#await getProfilePicture(post.u.split(": ")[0])}
+                        {#await getProfilePicture(post.p.split(": ")[0])}
                             <div class="rounded-circle bg-light" style="width: 50; height: 50;"></div>
                         {:then pfp}
                             <img class="rounded-circle bg-light" width="50" height="50" alt="{post.u}'s profile picture" src={pfp} onerror="this.src='./pfp/icon_err.svg'">
@@ -105,7 +136,7 @@
                     <p>{post.p.slice(post.p.indexOf(": ") + 1)}</p>
                 {:else if post.u == "Revower"}
                     <div class="float-start me-3">
-                        {#await getProfilePicture(post.u.split(": ")[0])}
+                        {#await getProfilePicture(post.p.split(": ")[0])}
                             <div class="rounded-circle bg-light" style="width: 50; height: 50;"></div>
                         {:then pfp}
                             <img class="rounded-circle bg-light" width="50" height="50" alt="{post.u}'s profile picture" src={pfp} onerror="this.src='./pfp/icon_err.svg'">
@@ -159,7 +190,7 @@
 
 <style>
     .spinner {
-        color: var(--sidebar-colour);
+        color: var(--dark-light);
     }
 
     #input {
@@ -170,5 +201,14 @@
     #input-post {
         margin-right: 0.8vw;
         margin-bottom: 2vh;
+    }
+
+    .chat-info {
+        background-color: var(--sidebar-bg);
+        position: fixed;
+        top: 0;
+        width: 100vw;
+        margin-right: 10vw;
+        margin-left: -1.2vw;
     }
 </style>
